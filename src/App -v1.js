90b3851303d4e2c6ca17089ca27723f8cf4ad18f
Himanshu <69390482+himanshukharwar@users.noreply.github.com>
2023-08-06 20:42:07 +0530

    Fetched API and Loading State

--- src/App -v1.js
+++ src/App -v1.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const tempMovieData = [
   {
@@ -50,26 +50,9 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "ea1c6524";
-const query = "interstellar"
-
 export default function App() {
-  const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
-
-  useEffect(function () {
-    async function fetchMOvies() {
-      setIsLoading(true)
-      const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
-      );
-      const data = await res.json();
-      setMovies(data.Search);
-      setIsLoading(false);
-    }
-    fetchMOvies();
-  }, []);
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
@@ -79,7 +62,7 @@ export default function App() {
       </NavBar>
       <Main>
         <Box>
-         {isLoading ? <Loader /> : <MovieList movies={movies} />}
+          <MovieList movies={movies} />
         </Box>
         <Box>
           <>
@@ -90,10 +73,6 @@ export default function App() {
       </Main>
     </>
   );
-}
-
-function Loader() {
-  return <p className="loader">Loading...</p>
 }
 
 function NavBar({ children }) {
